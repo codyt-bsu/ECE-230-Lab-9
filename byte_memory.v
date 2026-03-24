9c6e5277802e1b3c6f4a48a8f7551db4c9a86497
CamoRanger12 <camorangercodyp.t@gmail.com>
2026-03-24 11:17:02 -0600

    stuff

--- byte_memory.v
+++ byte_memory.v
@@ -7,19 +7,23 @@ module byte_memory(
     // Herein, implement D-Latch style memory
     // that stores the input data into memory
     // when store is high
-
     
-    genvar i;
-    generate 
-        for (i = 0; i < 8 ; i = i + 1)
-        begin
-            d_latch inst(
-                .D(data[i]),
-                .E(store),
-                .Q(memory[i])
-            );
-        end
-    endgenerate
+    always @(store, data) begin
+        if (store)
+            memory <= data;
+    end
+    
+//    genvar i;
+//    generate 
+//        for (i = 0; i < 8 ; i = i + 1)
+//        begin
+//            d_latch inst(
+//                .D(data[i]),
+//                .E(store),
+//                .Q(memory[i])
+//            );
+//        end
+//    endgenerate
 
 
 
