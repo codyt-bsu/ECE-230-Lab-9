35e01086fc0653e4090ba8ac0c36d485b62a05c9
HolmanCanyon <canyonholman@u.boisestate.edu>
2026-03-24 16:35:39 +0000

    Implemented Store demux

--- memory_system.v
+++ memory_system.v
@@ -5,6 +5,44 @@ module memory_system(
     output [7:0] memory
 );
 
+    wire store0;
+    wire store1;
+    wire store2;
+    wire store3;
+
+    demultiplexer_1bit storeDemux(
+        .data(store)
+        .sel(addr)
+        .A(store0)
+        .B(store1)
+        .C(store2)
+        .D(store3)
+    );
+
+    byte_memory mem0(
+        .store(store0)
+        .data(data)
+        .memory(memory)
+    );
+
+    byte_memory mem1(
+        .store(store1)
+        .data(data)
+        .memory(memory)
+    );
+
+    byte_memory mem2(
+        .store(store2)
+        .data(data)
+        .memory(memory)
+    );
+
+    byte_memory mem3(
+        .store(store3)
+        .data(data)
+        .memory(memory)
+    );
+    
     // This should instantiate 4 instances of
     // byte_memory, and then demultiplex
     // data and store into the one selected by
